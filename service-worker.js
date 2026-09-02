9e51b3e76bc0bf7a2572b70716886f2f0a8f9ab5
chenyumeng0310-commits <chenyumeng0310@gmail.com>
2026-09-02 11:55:17 +0800

    支持添加更多目标步骤

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'yumeng-manbu-v2';
+const CACHE = 'yumeng-manbu-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
